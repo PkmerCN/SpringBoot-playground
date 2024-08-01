b23e037d1097746ba09d76ad156930be22f9cdcb
zhuangzhuang-huang <1193094618@qq.com>
2024-08-01 23:09:43 +0800

    测试我们的依赖类

---
+++ test-app-scheduler-xxljob/src/main/java/org/hzz/testxxljob/TestXxlJobApplication.java
@@ -0,0 +1,27 @@
+package org.hzz.testxxljob;
+
+//import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.hzz.scheduler.autoconfig.XxlJobAutoConfig;
+import org.hzz.scheduler.xxljob.XxlJobProps;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/8/1
+ */
+@SpringBootApplication
+public class TestXxlJobApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appContext = SpringApplication.run(TestXxlJobApplication.class, args);
+
+        String[] beanNamesXxlJobAutoConfig = appContext.getBeanNamesForType(XxlJobAutoConfig.class);
+        String[] beanNamesForXxlJobProps = appContext.getBeanNamesForType(XxlJobProps.class);
+        System.out.println(beanNamesXxlJobAutoConfig.length);
+        System.out.println(beanNamesForXxlJobProps.length);
+//        XxlJobSpringExecutor
+
+    }
+}
